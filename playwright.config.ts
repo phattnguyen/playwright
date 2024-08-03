5db491ab7e828bff12fda00e0149a7fa0c5a1357
phattnguyen <n.phat@elsevier.com>
2024-08-03 22:04:59 +0700

    update master branch

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  // globalSetup: "./global.setup",
+  globalSetup: "./global.setup",
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
