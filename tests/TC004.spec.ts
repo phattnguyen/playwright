7e6788f39d266403afa491274c5de8292aeb84fc
phattnguyen <n.phat@elsevier.com>
2024-08-05 10:55:31 +0700

    enhance TC4

--- tests/TC004.spec.ts
+++ tests/TC004.spec.ts
@@ -3,26 +3,29 @@ const assert = require('assert');
 
 
 test('test iframe', async ({ page }) => {
-  const iframeTabLocator = "ul[class='resp-tabs-list '] > li[id='iFrame']";
+  test.setTimeout(150_000);
+
+  const iframeTabLocator = "[id=iFrame]";
   const iframeLocator = "iframe[name='globalSqa']";
   const iframeButtonSearchLocator = "button[class='button_search']";
   const searchResult = 'Sorry, no posts matched your criteria.';
 
   await page.goto('https://www.globalsqa.com/demo-site/frames-and-windows/');
-
+  await page.waitForLoadState('networkidle');
   // click iframe tab
-  await page.click(iframeTabLocator);
+  await page.locator(iframeTabLocator).click();
 
   // switch iframe and search
   await page.frameLocator(iframeLocator).first().getByPlaceholder("Search...").fill("Playwright");
   await page.frameLocator(iframeLocator).first().locator(iframeButtonSearchLocator).click();
 
   // verify text present
+  await page.waitForLoadState('networkidle');
   const iframeEle = await page.waitForSelector(iframeLocator);
   const iframe = await iframeEle.contentFrame();
 
   // wait for frame to fully loaded
-  await iframe?.waitForLoadState('load');
+  await iframe?.waitForLoadState('networkidle');
   const iframeContent = await iframe?.textContent('body');
   await assert.strictEqual(iframeContent?.includes(searchResult),true);
   })
